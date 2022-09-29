1fa7cc87e54c90b32bc1d5982f1a373b0609851d
Sky <Sky.Rivera89@gmail.com>
2022-09-29 20:11:48 +0330

    Add Pallet

---
+++ Code/Smart Vocabulary/lib/Utils/Pallet.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Pallet {
+
+  static Color lightYellow = Color(0xFFF5ECCD);
+  static Color lightOrange = Color(0xFFFCC8C2);
+  static Color Orange = Color(0xFFFF87AB);
+  static Color Pink = Color(0xFFFF47DA);
+  static Color Purple = Color(0xFF1B075E);
+
+}
